064e14c4deeac833d05a6c908b83c17a727c1085
idkdevgenerate <BeatCanvasMusicYT@gmail.com>
2025-03-13 19:14:51 +0000

    Update migrate.ts

--- lib/db/migrate.ts
+++ lib/db/migrate.ts
@@ -7,23 +7,23 @@ config({
   path: '.env.local',
 });
 
-const runMigrate = async () => {
-  if (!process.env.POSTGRES_URL) {
-    throw new Error('POSTGRES_URL is not defined');
-  }
+//const runMigrate = async () => {
+  //if (!process.env.POSTGRES_URL) {
+    //throw new Error('POSTGRES_URL is not defined');
+  //}
 
-  const connection = postgres(process.env.POSTGRES_URL, { max: 1 });
-  const db = drizzle(connection);
+  //const connection = postgres(process.env.POSTGRES_URL, { max: 1 });
+  //const db = drizzle(connection);
 
-  console.log('⏳ Running migrations...');
+  //console.log('⏳ Running migrations...');
 
-  const start = Date.now();
-  await migrate(db, { migrationsFolder: './lib/db/migrations' });
-  const end = Date.now();
+  //const start = Date.now();
+  //await migrate(db, { migrationsFolder: './lib/db/migrations' });
+  //const end = Date.now();
 
-  console.log('✅ Migrations completed in', end - start, 'ms');
-  process.exit(0);
-};
+  //console.log('✅ Migrations completed in', end - start, 'ms');
+  //process.exit(0);
+//};
 
 runMigrate().catch((err) => {
   console.error('❌ Migration failed');
